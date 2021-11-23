fc96ce1f4e8d797a048f07d32202b537f3711cf5
nahid-sheikh <nahidsheikh.shariar.com>
2021-11-23 10:23:34 +0600

    add hover scale style in photo cart

--- utils/PhotoCart/PhotoCart.js
+++ utils/PhotoCart/PhotoCart.js
@@ -3,7 +3,13 @@ import Image from "next/image";
 const PhotoCart = ({ img, headerText, title, actionBtn }) => {
   return (
     <div className="w-full h-full relative overflow-hidden group">
-      <Image src={img} alt="photo" layout="fill" objectFit="cover" className="hover:scale-110 duration-200" />
+      <Image
+        src={img}
+        alt="photo"
+        layout="fill"
+        objectFit="cover"
+        className="hover:scale-110 duration-200"
+      />
       <div className="absolute bottom-0 w-full h-[30%] bg-black bg-opacity-50 px-8 hover:scale-105 duration-200">
         <div>
           {headerText}
